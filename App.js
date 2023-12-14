ba2107439ce806220f4d9130e19ca574f1ed4616
Cauezins <109925592+Cauezins@users.noreply.github.com>
2023-12-14 09:25:42 -0300

    aiiiii

--- App.js
+++ App.js
@@ -1,34 +1,149 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Image, Pressable} from 'react-native';
 import { NavigationContainer, useNavigation  } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen() {
+function HomeScreen(){
   const navigation = useNavigation();
-  const [text, onChangeText] = React.useState('');
   return(
-    <View style={styles.container}>
-    <Text style={styles.text} >ARIEL DE QUISSAMÃ</Text>
-    <TextInput
-        placeholder="E-mail"
-        keyboardType="email-address"
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-      />
-    <Button style={styles.button} title='cadastro' onPress={()=>{navigation.navigate('Cadastro')}}/>
-    <Text>{text}</Text>
-    <StatusBar style="auto" />
+    <View style={styles.containerHome}>
+      <View style={styles.butons}>
+        <Pressable onPress={()=>{navigation.navigate('Cadastro')}} style={styles.button} >
+          <Text >Cadastre-se</Text>
+        </Pressable>
+        <Pressable onPress={()=>{navigation.navigate('Login')}} style={styles.button}>
+          <Text >Login</Text>
+        </Pressable>
+      </View>
+      <StatusBar style="auto" />
     </View>
   )
 }
-function HomeCadastro() {
+
+function CadScreen() {
+  const navigation = useNavigation();
+  const [emailCad, setEmail] = React.useState('');
+  const [passwordCad, setPassword] = React.useState('');
   return(
     <View style={styles.container}>
-    <Text style={styles.text} >ARIEL DE QUISSAMÃ -- Cadastro</Text>
-    <Button style={styles.button} title='voltar' onPress={()=>{navigation.navigate('Home')}}/>
-    <StatusBar style="auto" />
+       <Image
+        source={{ uri: 'https://i.imgur.com/RJEu2o9.png' }}
+        style={styles.logo}
+      />
+      <TextInput
+        placeholder="Nome completo"
+        style={styles.input}
+        onChangeText={(text) => {}}
+      />
+      <TextInput
+        placeholder="Data de nascimento"
+        style={styles.input}
+        onChangeText={(text) => {}}
+      />
+      <TextInput
+        placeholder="Email"
+        keyboardType="email-address"
+        style={styles.input}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        placeholder="Senha"
+        style={styles.input}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      
+      <Pressable onPress={()=>{navigation.navigate('Login')}} style={styles.button}>
+        <Text >Cadastre-se</Text>
+      </Pressable>
+      
+      <Text style={styles.loginText}>Já possui uma conta?</Text>
+      <Text style={styles.loginLinkText} onPress={()=>{navigation.navigate('Login')}}>Faça login</Text>
+
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+function LoginScreen() {
+  const navigation = useNavigation();
+  const [emailCad, setEmail] = React.useState('');
+  const [passwordCad, setPassword] = React.useState('');
+  return(
+    <View style={styles.container}>
+       <Image
+        source={{ uri: 'https://i.imgur.com/RJEu2o9.png' }}
+        style={styles.logo}
+      />
+      <TextInput
+        placeholder="Email"
+        keyboardType="email-address"
+        style={styles.input}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        placeholder="Senha"
+        style={styles.input}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      
+      <Pressable onPress={()=>{navigation.navigate('Tarefas')}} style={styles.button}>
+        <Text>Login</Text>
+      </Pressable>
+      
+      <Text style={styles.loginText}>Esqueceu a senha?</Text>
+      <Text style={styles.loginLinkText} onPress={()=>{navigation.navigate('Esqueci a senha')}}>Esqueci a senha</Text>
+      
+      <Text style={styles.loginText}>Não possui uma conta?</Text>
+      <Text style={styles.loginLinkText} onPress={()=>{navigation.navigate('Cadastro')}}>Cadastre-se</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+function RecSenhaScreen() {
+  const navigation = useNavigation();
+  const [emailRec, setEmail] = React.useState('');
+  const [passwordRec, setPassword] = React.useState('');
+  const [codigoRec, setCodigo] = React.useState('');
+  return(
+    <View style={styles.container}>
+       <Image
+        source={{ uri: 'https://i.imgur.com/RJEu2o9.png' }}
+        style={styles.logo}
+      />
+      <TextInput
+        placeholder="Email"
+        keyboardType="email-address"
+        style={styles.input}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        placeholder="Nova Senha"
+        style={styles.input}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <TextInput
+        placeholder="Codigo de confirmação"
+        style={styles.input}
+        onChangeText={setCodigo}
+      />
+      
+      <Pressable onPress={()=>{navigation.navigate('Login')}} style={styles.button}>
+        <Text >Trocar Senha</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+function TarefasScreen() {
+  const navigation = useNavigation();
+  return(
+    <View style={styles.container}>
+      <Text>ENTROUUU</Text>
     </View>
   )
 }
@@ -41,13 +156,66 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Cadastro" component={HomeCadastro} />
+        <Stack.Screen name="Cadastro" component={CadScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Esqueci a senha" component={RecSenhaScreen} />
+        <Stack.Screen name="Tarefas" component={TarefasScreen} />
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+ },
+ containerHome: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: "flex-end",
+  borderWidth: 1,
+},
+ logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 30,
+ },
+ butons: {
+  width: '80%',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: "space-around",
+  marginBottom: 20
+ },
+ button: {
+  width: 190,
+  height: 40,
+  borderWidth: 1,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: 10,
+ },
+ input: {
+    width: '80%',
+    height: '40px',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+    borderRadius: 10,
+ },
+ loginText: {
+    marginTop: 30,
+ },
+ loginLinkText: {
+    color: 'blue',
+    marginTop: 10,
+ },
   
 });
